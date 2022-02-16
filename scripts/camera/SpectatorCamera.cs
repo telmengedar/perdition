3810fdd6826a56659daad45e1221f4e26736f46d
Toni Gottschall <toni.gottschall@nightlycode.eu>
2022-02-17 00:24:54 +0100

    spectator camera in scene

---
+++ scripts/camera/SpectatorCamera.cs
@@ -0,0 +1,39 @@
+using Godot;
+
+namespace Perdition.scripts.camera {
+    public class SpectatorCamera : Camera {
+        
+        // Called when the node enters the scene tree for the first time.
+        public override void _Ready() {
+            //SetProcess(false);
+        }
+
+        public override void _Input(InputEvent @event) {
+            base._Input(@event);
+            if (@event is InputEventMouseMotion mouseMotion) {
+                Rotation += new Vector3(-mouseMotion.Relative.y * 0.002f, -mouseMotion.Relative.x * 0.002f, 0.0f);
+            }
+
+            if (@event is InputEventKey keyEvent) {
+                
+            }
+        }
+
+        // Called every frame. 'delta' is the elapsed time since the previous frame.
+        public override void _Process(float delta)
+        {
+            Vector3 direction=Vector3.Zero;
+
+            if (Input.IsKeyPressed((int)KeyList.A))
+                direction -= Transform.basis.x;
+            if (Input.IsKeyPressed((int)KeyList.D))
+                direction += Transform.basis.x;
+            if (Input.IsKeyPressed((int)KeyList.W))
+                direction -= Transform.basis.z;
+            if (Input.IsKeyPressed((int)KeyList.S))
+                direction += Transform.basis.z;
+
+            Translation += direction.Normalized() * delta * 5.0f;
+        }
+    }
+}
